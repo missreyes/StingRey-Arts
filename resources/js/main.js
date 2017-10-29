b4c8bcb57dc5fa80a8b0a6df7473344882e47ab7
missreyes <username@users.noreply.github.com>
2017-10-29 15:12:49 -0700

    Changed menu location and function, added links, readjusted colors

--- resources/js/main.js
+++ resources/js/main.js
@@ -11,16 +11,29 @@ $(document).ready(function(){
     pauseOnHover: false,
   });
 
-  var userXcrolled = 0;
-  $(window).scroll (function() {
-    var scrollTop = $(this).scrollTop();
-    if (scrollTop - userXcrolled > 50) {
-      var newZeight = $('.navbar').css('height');
-      $('.navbar').animate({top: '-' + newZeight}, 150);
-      userXcrolled = scrollTop;
-    } else if  (userXcrolled - scrollTop  > 50) {
-      $('.navbar').animate({top: '0px'}, 150);
-      userXcrolled = scrollTop;
-    }
+  // var userXcrolled = 0;
+  // $(window).scroll (function() {
+  //   var scrollTop = $(this).scrollTop();
+  //   if (scrollTop - userXcrolled > 50) {
+  //     var newZeight = $('.navbar').css('height');
+  //     $('.navbar').animate({top: '-' + newZeight}, 150);
+  //     userXcrolled = scrollTop;
+  //   } else if  (userXcrolled - scrollTop  > 50) {
+  //     $('.navbar').animate({top: '0px'}, 150);
+  //     userXcrolled = scrollTop;
+  //   }
+
+  $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the
+        //nav bar to stick.
+        console.log($(window).scrollTop())
+      if ($(window).scrollTop() > 700) {
+        $('.navbar').addClass('navbar-fixed');
+      }
+      if ($(window).scrollTop() < 701) {
+        $('.navbar').removeClass('navbar-fixed');
+      }
+
   });
 });
